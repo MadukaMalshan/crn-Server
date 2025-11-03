240146f380fde9ec75bdaa567ccbbe142ff3d9a5
MadukaMalshan <malshanmaduka8@gmail.com>
2025-11-03 15:09:08 +0530

    Create RestController

---
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -0,0 +1,12 @@
+package edu.icet.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+    @GetMapping
+    public String getName(){
+        return "Maduka";
+    }
+}
