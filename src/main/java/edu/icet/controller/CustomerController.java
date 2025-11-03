f86d04c65333234f0e810703d73cfcc9334abc8f
MadukaMalshan <malshanmaduka8@gmail.com>
2025-11-03 15:52:32 +0530

    Create model class

--- src/main/java/edu/icet/controller/CustomerController.java
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package edu.icet.controller;
 
+import edu.icet.model.Customer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/customer")
 public class CustomerController {
     @GetMapping("/name")
-    public String getName(){
+    public String getName() {
         return "Maduka";
     }
 
     @GetMapping("/age")
-    public Integer getAge(){
+    public Integer getAge() {
         return 22;
+    }
+
+
+    @GetMapping("/get-customer")
+    public Customer getCustomer(){
+        return new Customer(
+                "C001",
+                "Maduka",
+                "Galle",
+                25000.00
+        );
     }
 }
